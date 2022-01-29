45046dbeb7ad6c355721d481771236e2b5100e4e
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-01-29 16:50:39 -0600

    01/29/2022 commit

---
+++ ex02.py
@@ -0,0 +1,10 @@
+# a comment, this is so you can read your program later.
+# anything after the # is ignored by python.
+
+print("you can have code like this.") #and the comment after is ignored.
+
+#you can also use a comment to disable or comment out code:
+#print('this won't run.') #this line is commented out
+
+print('this will run.')
+print('hey # there.') #the # is not ignored because it is in a string
